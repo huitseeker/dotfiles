ac40ee56860418a0c7a98944c0b4ff0b38a7060c
François Garillot <francois@garillot.net>
2020-10-15 12:50:05 -0700

    tweak mirai params

--- bin/check-mirai.sh
+++ bin/check-mirai.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 outfile="mirai-${PWD##*/}"
 
 rustup override set $(cat ~/tmp/MIRAI/rust-toolchain)
-RUSTFLAGS="-Z always_encode_mir" cargo build
+RUSTFLAGS="-Z always_encode_mir" cargo build --tests
 find ./ -iname 'lib.rs' -exec touch '{}' \;
 
-export DYLD_LIBRARY_PATH=~/.rustup/toolchains/+$(cat ~/tmp/MIRAI/rust-toolchain)-x86_64-unknown-linux-gnu/lib
-RUSTC_WRAPPER=mirai RUSTFLAGS="-Z always_encode_mir" cargo build 2>&1 | tee $HOME/tmp/$outfile
+export DYLD_LIBRARY_PATH=~/.rustup/toolchains/$(cat ~/tmp/MIRAI/rust-toolchain)-x86_64-unknown-linux-gnu/lib
+RUSTC_WRAPPER=mirai RUSTFLAGS="-Z always_encode_mir" cargo build "$@" 2>&1 | tee $HOME/tmp/$outfile
